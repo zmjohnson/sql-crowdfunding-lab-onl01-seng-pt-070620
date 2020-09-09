f71fcb4b61da07d5c73bc7f41fcbb64ef77e80b2
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-09-09 01:02:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,11 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "Write your SQL query Here"
+  "SELECT projects.title, (projects.funding_goal - SUM(pledges.amount)) * -1 AS over_goal FROM projects
+    INNER JOIN pledges
+      ON projects.id = pledges.project_id
+    GROUP BY projects.title
+    HAVING over_goal >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
