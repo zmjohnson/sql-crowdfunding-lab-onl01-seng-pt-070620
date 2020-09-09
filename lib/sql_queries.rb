310d0ef306457ea2f61cb53bffb6b712d10b5c16
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-09-09 01:02:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,5 +27,8 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "Write your SQL query Here"
+  "SELECT projects.category, SUM(pledges.amount) AS total_pledges FROM projects
+    INNER JOIN pledges
+      ON projects.id = pledges.project_id
+    WHERE category = 'books';"
 end
